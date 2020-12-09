6144afc883629a6ba3e406bacdc9725c263fcedb
Daniel Häggström <daniel.hpf@hotmail.com>
2020-12-09 23:42:35 +0100

    introduce la simulación en una función, para minimizar problemas de scope

--- src/main.py
+++ src/main.py
@@ -410,124 +410,128 @@ def daily_test(people_list, masks_dict, data_db):
 
 
 # iniciamos la ejecución
-for handler in logging.root.handlers[:]:
-    logging.root.removeHandler(handler)
-logging.basicConfig(level=logging.DEBUG, filename='log.txt', filemode='w', format='%(message)s')
+def main():
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    logging.basicConfig(level=logging.DEBUG, filename='log.txt', filemode='w', format='%(message)s')
 
-# parámetros de la simulación
-random.seed(4862)
-n = 5000  # número de gente
-person_types = ["Worker", "Student", "Stay at home"]  # probabilidad de tipo de persona
-person_weights = [4, 3, 2]
+    # parámetros de la simulación
+    random.seed(4862)
+    n = 5000  # número de gente
+    person_types = ["Worker", "Student", "Stay at home"]  # probabilidad de tipo de persona
+    person_weights = [4, 3, 2]
 
-start_date = datetime(2020, 3, 1)
-end_date = datetime(2020, 12, 1)
+    start_date = datetime(2020, 3, 1)
+    end_date = datetime(2020, 12, 1)
 
-Hospital.mod_infection = 5
-Pharmacy.mod_infection = 1.3
-Learning_Center.mod_infection = 1.1
-Workplace.mod_infection = 0.8
-Market.mod_infection = 1
-Mall.mod_infection = 1.2
+    Hospital.mod_infection = 5
+    Pharmacy.mod_infection = 1.3
+    Learning_Center.mod_infection = 1.1
+    Workplace.mod_infection = 0.8
+    Market.mod_infection = 1
+    Mall.mod_infection = 1.2
 
-Home.leave_chance = 0.3
-Hospital.leave_chance = 0.7
-Pharmacy.leave_chance = 0.8
-Learning_Center.leave_chance = 0.1
-Workplace.leave_chance = 0.1
-Market.leave_chance = 0.6
-Mall.leave_chance = 0.4
+    Home.leave_chance = 0.3
+    Hospital.leave_chance = 0.7
+    Pharmacy.leave_chance = 0.8
+    Learning_Center.leave_chance = 0.1
+    Workplace.leave_chance = 0.1
+    Market.leave_chance = 0.6
+    Mall.leave_chance = 0.4
 
-Person.p_covid = 0.01  # probabilidad de tener covid al empezar la simulacion
-Person.p_infect = 0.0001  # probabilidad de ser infectado durante una hora por una persona positiva
-Person.p_mortality = 0.034  # probabilidad de fallecer por la infección
-Person.hospital_chance = 0.001  # probabilidad de ir al hospital cuando se tiene un pcr positivo
-Person.immunity = 30  # días de inmunidad después de recuperarte
-Person.incubation_time = 5  # tiempo de incubación
-Person.current_datetime = start_date  # debe ser actualizada cada hora
+    Person.p_covid = 0.01  # probabilidad de tener covid al empezar la simulacion
+    Person.p_infect = 0.0001  # probabilidad de ser infectado durante una hora por una persona positiva
+    Person.p_mortality = 0.034  # probabilidad de fallecer por la infección
+    Person.hospital_chance = 0.001  # probabilidad de ir al hospital cuando se tiene un pcr positivo
+    Person.immunity = 30  # días de inmunidad después de recuperarte
+    Person.incubation_time = 5  # tiempo de incubación
+    Person.current_datetime = start_date  # debe ser actualizada cada hora
 
-# creamos los edificios en Person.building_dict
-# contiene keys con los tipos de edificios, y los valores son listas de los edificios de cada tipo
-Person.building_dict = {"Trabajo": [Workplace("Trabajo 1", 1, n * 0.1),
-                                    Workplace("Trabajo 2", 1, n * 0.1),
-                                    Workplace("Trabajo 3", 2, n * 0.2),
-                                    Workplace("Trabajo 4", 2, n * 0.15)],
-                        "Hospital": [Hospital("Hospital 1", 2, n * 0.35, n * 0.2),
-                                     Hospital("Hospital 2", 3, n * 0.4, n * 0.3)],
-                        "Farmacia": [Pharmacy("Farmacia", 3, n * 0.2)],
-                        "Centro Educativo": [Learning_Center("Colegio 1", 2, n * 0.2),
-                                             Learning_Center("Colegio 2", 2, n * 0.2),
-                                             Learning_Center("Universidad", 3, n * 0.3)],
-                        "Supermercado": [Market("Supermercado", 3, n * 0.2)],
-                        "Centro Comercial": [Mall("Centro Comercial", 1, n * 0.5)],
-                        "Home": [Person.home]
-                        }
+    # creamos los edificios en Person.building_dict
+    # contiene keys con los tipos de edificios, y los valores son listas de los edificios de cada tipo
+    Person.building_dict = {"Trabajo": [Workplace("Trabajo 1", 1, n * 0.1),
+                                        Workplace("Trabajo 2", 1, n * 0.1),
+                                        Workplace("Trabajo 3", 2, n * 0.2),
+                                        Workplace("Trabajo 4", 2, n * 0.15)],
+                            "Hospital": [Hospital("Hospital 1", 2, n * 0.35, n * 0.2),
+                                         Hospital("Hospital 2", 3, n * 0.4, n * 0.3)],
+                            "Farmacia": [Pharmacy("Farmacia", 3, n * 0.2)],
+                            "Centro Educativo": [Learning_Center("Colegio 1", 2, n * 0.2),
+                                                 Learning_Center("Colegio 2", 2, n * 0.2),
+                                                 Learning_Center("Universidad", 3, n * 0.3)],
+                            "Supermercado": [Market("Supermercado", 3, n * 0.2)],
+                            "Centro Comercial": [Mall("Centro Comercial", 1, n * 0.5)],
+                            "Home": [Person.home]
+                            }
 
-# creamos la gente
-Person.alive = []  # borramos la gente de la ejecución anterior
-Person.dead = []
-for num in range(n):
-    person_type = random.choices(population=person_types, weights=person_weights)[0]
-    if person_type == "Worker":
-        Worker(num)
-    elif person_type == "Student":
-        Student(num)
-    elif person_type == "Stay at home":
-        Stay_at_home(num)
-Person.home.occupancy = Person.alive.copy()
-poblacion = Person.alive.copy()
+    # creamos la gente
+    Person.alive = []  # borramos la gente de la ejecución anterior
+    Person.dead = []
+    for num in range(n):
+        person_type = random.choices(population=person_types, weights=person_weights)[0]
+        if person_type == "Worker":
+            Worker(num)
+        elif person_type == "Student":
+            Student(num)
+        elif person_type == "Stay at home":
+            Stay_at_home(num)
+    Person.home.occupancy = Person.alive.copy()
+    poblacion = Person.alive.copy()
 
-# horas umbrales
-hora_limite_1 = time(22, 0, 0)
-hora_limite_2 = time(8, 0, 0)
+    # horas umbrales
+    hora_limite_1 = time(22, 0, 0)
+    hora_limite_2 = time(8, 0, 0)
 
-# obtenemos una lista de edificios
-edificios = []
-for value in Person.building_dict.values():
-    edificios.extend(value)
+    # obtenemos una lista de edificios
+    edificios = []
+    for value in Person.building_dict.values():
+        edificios.extend(value)
 
-# creamos los dataframes
-mascarillas = {"id": [mask for mask in range(n)]}
-datos = {"Fallecidos": [], "Contagiados": [], "Inmunizados": [], "Fecha": []}
+    # creamos los dataframes
+    mascarillas = {"id": [mask for mask in range(n)]}
+    datos = {"Fallecidos": [], "Contagiados": [], "Inmunizados": [], "Fecha": []}
 
-# iniciamos la simulación
-print("Empezando simulación.")
-for single_date in daterange(start_date, end_date):
-    Person.current_datetime = single_date
-    for persona in Person.alive:
-        persona.update()
-    for edificio in edificios:
-        edificio.update()
-
-    # comprobamos si son entre las 22:00 y las 08:00 inclusive
-    hour = time(single_date.hour, 0, 0)
-
-    if hour == hora_limite_1:
-        # todos a casa
+    # iniciamos la simulación
+    print("Empezando simulación.")
+    for single_date in daterange(start_date, end_date):
+        Person.current_datetime = single_date
         for persona in Person.alive:
-            persona.back_home()
-        # hacemos tests
-        datos["Fecha"].append(single_date.date())
-        daily_test(poblacion, mascarillas, datos)
-        continue
-
-    if time_in_range(hora_limite_1, hora_limite_2, hour):
-        # nadie se mueve
-        pass
-    else:
-        # todos se mueven
-        for persona in Person.alive:
-            persona.move()
+            persona.update()
         for edificio in edificios:
-            edificio.contact()
+            edificio.update()
 
-print("Guardando datos.")
-# guardamos los datos
-for edificio in edificios:
-    df = pandas.DataFrame.from_dict(edificio.db)
-    df.to_csv(edificio.name + ".csv", sep=";")
-mascarillas = pandas.DataFrame.from_dict(mascarillas).set_index("id")
-mascarillas.to_csv("mascarillas.csv", sep=";")
-datos_df = pandas.DataFrame.from_dict(datos).set_index("Fecha")
-datos_df.to_csv("datos_diarios.csv", sep=";")
-print("Terminado.")
+        # comprobamos si son entre las 22:00 y las 08:00 inclusive
+        hour = time(single_date.hour, 0, 0)
+
+        if hour == hora_limite_1:
+            # todos a casa
+            for persona in Person.alive:
+                persona.back_home()
+            # hacemos tests
+            datos["Fecha"].append(single_date.date())
+            daily_test(poblacion, mascarillas, datos)
+            continue
+
+        if time_in_range(hora_limite_1, hora_limite_2, hour):
+            # nadie se mueve
+            pass
+        else:
+            # todos se mueven
+            for persona in Person.alive:
+                persona.move()
+            for edificio in edificios:
+                edificio.contact()
+
+    print("Guardando datos.")
+    # guardamos los datos
+    for edificio in edificios:
+        df = pandas.DataFrame.from_dict(edificio.db)
+        df.to_csv(edificio.name + ".csv", sep=";")
+    mascarillas = pandas.DataFrame.from_dict(mascarillas).set_index("id")
+    mascarillas.to_csv("mascarillas.csv", sep=";")
+    datos_df = pandas.DataFrame.from_dict(datos).set_index("Fecha")
+    datos_df.to_csv("datos_diarios.csv", sep=";")
+    print("Terminado.")
+
+
+main()
